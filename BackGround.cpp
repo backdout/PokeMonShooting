0825b327e7e92a77bed1391e2e8c4bb27c1a5916
backdout <69623062+backdout@users.noreply.github.com>
2020-08-18 18:18:04 +0900

    Add files via upload

---
+++ BackGround.cpp
@@ -0,0 +1,84 @@
+#include "BackGround.h"
+#include "Image.h"
+#include <ctime>
+
+
+HRESULT BackGround::Init()
+{
+	posX = WINSTART_X;
+	posY = WINSTART_Y + 38;
+	srand(time(NULL));
+	BgNum = rand()%6;
+	img = new Image();
+	switch (BgNum)
+	{
+	case 0:
+		img->Init("Image/BG_horz.bmp", posX, posY, 6016, 224, 8, 1);
+		break;
+	case 1:
+			img->Init("Image/2_horz.bmp",posX, posY, 6008, 224, 8, 1);
+			break;
+	case 2:
+			img->Init("Image/3_horz.bmp",posX, posY, 6016, 224, 8, 1);
+			break;
+	case 3:
+			img->Init("Image/5_horz.bmp",posX, posY, 6000, 224, 8, 1);
+			break;
+	case 4:
+			img->Init("Image/6_horz.bmp",posX, posY, 6016, 224, 8, 1);
+			break;
+	case 5:
+			img->Init("Image/7_horz.bmp",posX, posY, 6016, 224, 8, 1);
+			break;
+	default:
+		break;
+	}
+	
+	currKeyFrameX = 0;
+	currKeyFrameY = 0;
+	elapsedFrame = 0;
+	scale = 2.0f;
+
+	return S_OK;
+}
+
+void BackGround::Release()
+{
+	img->Release();
+	delete img;
+}
+
+void BackGround::Update()
+{
+	elapsedFrame++;
+	if (elapsedFrame % 7 == 6 && currKeyFrameX != 8)
+	{
+		currKeyFrameX++;
+		elapsedFrame = 0;
+	}
+	else if (elapsedFrame % 3 == 2 && currKeyFrameX == 8)
+	{
+		currKeyFrameX = 0;
+		elapsedFrame = 0;
+	}
+
+}
+
+void BackGround::Render(HDC hdc)
+{
+	if (img)
+	{
+
+		img->FrameRender(hdc, posX, posY,
+			currKeyFrameX, currKeyFrameY, scale,false, 0, camerPosX);		
+	}
+}
+
+BackGround::BackGround()
+{
+}
+
+
+BackGround::~BackGround()
+{
+}
